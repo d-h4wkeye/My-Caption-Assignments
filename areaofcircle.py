7fb86275e1c3848b1e2fe32cd8ba6d3dd8dd037a
d-h4wkeye <106402415+d-h4wkeye@users.noreply.github.com>
2022-05-28 00:33:53 +0530

    Add files via upload

---
+++ areaofcircle.py
@@ -0,0 +1,5 @@
+#program to find area of circle
+pi=3.14
+r=int(input("Enter the radius of the circle"))
+area=pi*r*r
+print("The area of the circle with radius",r,"is:",area)
---
+++ typeoffile.py
@@ -0,0 +1,14 @@
+name=input("Enter Filename:")
+for i in range(0,len(name)):
+    if(name[i]=="."):
+        if(name[i+1::1]=="py"):
+            print("Python")
+        elif(name[i+1::1]=="txt"):
+            print("Textfile")
+        elif(name[i+1::1]=="c"):
+            print("C")
+        elif(name[i+1::1]=="exe"):
+            print("Executablefile")
+            
+        
+        
